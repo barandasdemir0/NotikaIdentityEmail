aaa236df20f4830f2e7a2b50137dcfd934b0e470
Baran <barandasdemir.bd@gmail.com>
2025-08-23 16:13:04 +0300

    CommentOperations

---
+++ NotikaIdentityEmail/Entities/Comment.cs
@@ -0,0 +1,12 @@
+﻿namespace NotikaIdentityEmail.Entities
+{
+    public class Comment
+    {
+        public int CommentID { get; set; }
+        public string CommentDetail { get; set; }
+        public DateTime CommentDate { get; set; }
+        public string CommentStatus { get; set; }
+        public string AppUserId { get; set; }
+        public AppUser AppUser { get; set; }
+    }
+}
